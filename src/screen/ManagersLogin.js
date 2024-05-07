3e6f7700fe4b5ffedce7035a8ba08676e8af897a
Dharmendra-1002 <dharmendra9942@gmail.com>
2024-05-07 16:09:26 +0530

    initialized git repository

--- src/screen/ManagersLogin.js
+++ src/screen/ManagersLogin.js
@@ -1,4 +1,4 @@
-import { View, Text ,TouchableOpacity} from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native';
@@ -8,41 +8,44 @@ const ManagersLogin = () => {
   const navigation = useNavigation();
   return (
     <SafeAreaView className=" flex-1">
-    <View className={`flex-1 justify-start items-center  `}>
-      <View className={`w-full h-full bg-white  rounded-lg shadow-lg p-4`}>
-        <View className={`flex justify-center items-center `}>
-          <View className={` w-11/12 h-70 bg-purple-200 m-10 rounded-lg shadow-lg p-4`}>
-            <TouchableOpacity onPress={() => navigation.navigate('Manging_Director')}>
-            <View>
-        <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Managing Director </Text>
+      <View className={`flex-1 justify-start items-center  `}>
+        <View className={`w-full h-full bg-white  rounded-lg shadow-lg p-4`}>
+          <View className={`flex justify-center items-center `}>
+            <View className={` w-11/12 h-70 bg-purple-200 m-10 rounded-lg shadow-lg p-4`}>
+              <TouchableOpacity onPress={() => navigation.navigate('Manging_Director')}>
+                <View>
+                  <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Managing Director </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('Authentication_Manger')}>
+                <View>
+                  <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Authentication Manager </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('FinanceManagerLogin')}>
+                <View>
+                  <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Finance Manager </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('MarketingExLogin')}>
+                <View>
+                  <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Marketing Head</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('MarketingExLogin')}>
+                <View>
+                  <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">State Manager</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('Marketing_Executive_Button')}>
+                <View>
+                  <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Marketing Executive </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
-            </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate('Authentication_Manger')}>
-    <View>
-        <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Authentication Manager </Text>
-      </View>
-    </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('FinanceManagerLogin')}>
-      <View>
-        <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Finance Manager </Text>
-      </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('MarketingExLogin')}>
-      <View>
-      <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Marketing Head</Text>
-      </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Marketing_Executive_Button')}>
-      <View>
-        <Text className="bg-violet-950 p-5 rounded-lg text-white m-2 text-center">Marketing Executive </Text>
-      </View>
-      </TouchableOpacity>
-   
-    
-</View>
-</View>
-</View>
-</View>
 
     </SafeAreaView>
 
